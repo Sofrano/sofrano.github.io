180b5d36482827fea0d380d68f0a493052a5a981
Dmitriy Safarov <kazdevelop@gmail.com>
2020-04-21 21:13:29 +0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var path = 'worker.js';
 var worker = new Worker(path);
 
 worker.onmessage = function (event) {
-    log('Message:' + JSON.stringify(event.data));
+    console.log('Message:' + JSON.stringify(event.data));
 };
 
 log('Initialized by path: ' + path);
