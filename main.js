25e6c38d774546fdfc8ed6480dae71bbd7a9fefc
Dmitriy Safarov <kazdevelop@gmail.com>
2020-04-22 00:24:57 +0600

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ var path = 'worker.js';
 var worker = new Worker(path);
 
 worker.onmessage = function (event) {
-    locListener.onSubmit({
+    var data = {
         'first_name': 'Davin',
         'last_name': 'Ahn',
         'mail': 'davin.ahn@ridi.com',
-    });
+    }
+    var str = JSON.stringify(data);
+    locListener.onSubmit(str);
     console.log('Message:' + JSON.stringify(event.data));
 };
 
